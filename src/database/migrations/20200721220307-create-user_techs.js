1fdef59c043a31ce26122313ad0ec39f5528e182
demoraes <gabrielmoraes_2013@outlook.com>
2020-07-21 19:59:06 -0400

    add TechController

--- src/database/migrations/20200721220307-create-user_techs.js
+++ src/database/migrations/20200721220307-create-user_techs.js
@@ -23,6 +23,10 @@ module.exports = {
         onUpdate: 'CASCADE',
         onDelete: 'CASCADE',
       },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
       updated_at: {
         type: Sequelize.DATE,
         allowNull: false,
